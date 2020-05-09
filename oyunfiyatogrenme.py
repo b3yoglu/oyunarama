f869ceb4e3674c29aca7196d07689b999a750c95
Ekrem Beyoğlu <63927815+b3yoglu@users.noreply.github.com>
2020-05-09 18:32:49 +0300

    Add files via upload

---
+++ oyunfiyatogrenme.py
@@ -0,0 +1,14 @@
+#Created By Beyoglu
+#usepython
+import requests
+import os
+from bs4 import BeautifulSoup
+print("********STEAM OYUN ARAMA V1.0 / BEYOGLU*************")
+oyunun_ismi = input("Oyun ismi girin:")
+r = requests.get('https://store.steampowered.com/search/?term=' + oyunun_ismi)
+cikti = BeautifulSoup(r.content, 'html.parser')
+cikti2 = cikti.find("div", {"class": "col search_price responsive_secondrow"})
+cikti3 = cikti.find("div", {"class": "col search_released responsive_secondrow"})
+cikti4 = os.linesep.join([s for s in cikti2.text.splitlines() if s])
+print("Oyun Fiyatı:" + cikti4.lstrip())
+print("Çıkış Tarihi:" + cikti3.text)
